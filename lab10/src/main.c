77c274c5f0895257b0e16c6f968521cdc1524167
xu1392 <xu1392@purdue.edu>
2020-11-18 23:34:51 -0500

    update

--- lab10/src/main.c
+++ lab10/src/main.c
@@ -13,6 +13,7 @@
 
 void advance_fattime(void);
 void command_shell(void);
+struct fifo input_fifo;  // input buffer
 
 // Write your subroutines below.
 void setup_usart5() {
@@ -51,8 +52,84 @@ void setup_usart5() {
   USART5 -> CR1 |= USART_CR1_UE;
   // Finally, you should wait for the TE and RE bits to be acknowledged.
   // This indicates that the USART is ready to transmit and receive.
-  while (((USART5 -> CR1) & (USART_CR1_TE | USART_CR1_RE)) != (USART_CR1_RE | USART_CR1_TE));
+  while (((USART5 -> ISR) & (USART_ISR_TEACK | USART_ISR_REACK)) !=
+          (USART_ISR_REACK | USART_ISR_TEACK));
 }
+
+int simple_putchar(int ch) {
+  // Wait for the USART5 ISR TXE to be set.
+  while (((USART5 -> ISR) & USART_ISR_TXE) != USART_ISR_TXE);
+  // Write the argument to the USART5 TDR (transmit data register).
+  USART5 -> TDR = (uint8_t) ch;
+  // Return the argument that was passed in. That's how putchar() is defined to work.
+  return ch;
+}
+
+int simple_getchar() {
+  int rdata = 0;
+  while ((USART5 -> ISR & USART_ISR_RXNE) != USART_ISR_RXNE);
+  rdata = USART5 -> RDR;
+  return rdata;
+}
+
+int better_putchar(int ch) {
+  if ((char)ch == '\n') {
+    // wait for the TXE bit to be set
+    while (((USART5 -> ISR) & USART_ISR_TXE) != USART_ISR_TXE);
+    USART5 -> TDR = (uint8_t) ch;
+    // write a '\r' to the TDR
+    while (((USART5 -> ISR) & USART_ISR_TXE) != USART_ISR_TXE);
+    USART5 -> TDR = (uint8_t) '\r';
+    return ch;
+  }
+  while (((USART5 -> ISR) & USART_ISR_TXE) != USART_ISR_TXE);
+  USART5 -> TDR = (uint8_t) ch;
+  return ch;
+}
+
+int better_getchar() {
+  int rdata = 0;
+  while ((USART5 -> ISR & USART_ISR_RXNE) != USART_ISR_RXNE);
+  rdata = USART5 -> RDR;
+  if (rdata == '\r') rdata = '\n';
+  return rdata;
+}
+
+int interrupt_getchar() {
+  int ch = 0;
+  if (fifo_newline(&input_fifo) == 0) {
+    asm volatile ("wfi");
+  }
+  else {
+    ch = fifo_remove(&input_fifo);
+  }
+  return ch;
+}
+
+void USART3_4_5_6_7_8_IRQHandler() {
+  // Check and clear the ORE flag.
+  if ((USART5 -> ISR & USART_ISR_ORE) == USART_ISR_ORE)
+  USART5 -> ICR |= USART_ICR_ORECF;
+  // Read the new character from the USART5 RDR.
+  int newch = USART5 -> RDR;
+  // Check if the input_fifo is full. If it is, return from the ISR.
+  // (Throw away the character.)
+  int rtv = fifo_full(&input_fifo);
+  if (rtv == 1) return;
+  // Call insert_echo_char() with the character.
+  insert_echo_char(newch);
+
+}
+
+int __io_putchar(int ch) {
+  return better_putchar(ch);
+}
+
+int __io_getchar(void) {
+  return line_buffer_getchar();
+}
+
+
 
 
 // Write your subroutines above.
@@ -65,36 +142,36 @@ int main()
     setup_usart5();
 
     // Uncomment these when you're asked to...
-    //setbuf(stdin, 0);
-    //setbuf(stdout, 0);
-    //setbuf(stderr, 0);
+    setbuf(stdin, 0);
+    setbuf(stdout, 0);
+    setbuf(stderr, 0);
 
     // Test 2.2 simple_putchar()
     //
-    //for(;;)
-    //    for(const char *t=testline; *t; t++)
-    //        simple_putchar(*t);
+    // for(;;)
+    //     for(const char *t=testline; *t; t++)
+    //         simple_putchar(*t);
 
     // Test for 2.3 simple_getchar()
     //
-    //for(;;)
-    //    simple_putchar( simple_getchar() );
+    // for(;;)
+    //     simple_putchar( simple_getchar() );
 
     // Test for 2.4 and 2.5 __io_putchar() and __io_getchar()
     //
-    //printf("Hello!\n");
-    //for(;;)
-    //    putchar( getchar() );
+    // printf("Hello!\n");
+    // for(;;)
+    //     putchar( getchar() );
 
     // Test for 2.6
     //
-    //for(;;) {
-    //    printf("Enter string: ");
-    //    char line[100];
-    //    fgets(line, 99, stdin);
-    //    line[99] = '\0'; // just in case
-    //    printf("You entered: %s", line);
-    //}
+    // for(;;) {
+    //     printf("Enter string: ");
+    //     char line[100];
+    //     fgets(line, 99, stdin);
+    //     line[99] = '\0'; // just in case
+    //     printf("You entered: %s", line);
+    // }
 
     // Test for 2.7
     //
